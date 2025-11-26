62913cf8a66d6a41decac93f2deb5346d391e5f3
abdUlkaderEng <abdulkaderbakdunes@gmail.com>
2025-11-26 17:35:27 +0300

    Refactor User Register Form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,6 +9,9 @@ import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
 import api from "../../libs/axios";
 import axios from "axios";
+import InputField from "@/Components/Form/InputField";
+import Button from "@/Components/Form/Button";
+import FormContainer from "@/Components/Form/FormContainer";
 
 const registerSchema = z
   .object({
@@ -27,7 +30,6 @@ type RegisterForm = z.infer<typeof registerSchema>;
 const RegisterPage = () => {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     handleSubmit,
@@ -60,78 +62,44 @@ const RegisterPage = () => {
     }
   };
   return (
-    <div className="animate-enter">
-      <SyriaMapBG />
-      <div className="min-h-screen flex items-center justify-center transition-colors duration-500">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="glass w-full max-w-sm p-6 space-y-5 text-center">
-          <h2 className="text-3xl font-semibold ">إنشاء حساب</h2>
+    <FormContainer title="التحقق من المشرف" onSubmit={handleSubmit(onSubmit)}>
+      <InputField
+        type="text"
+        placeholder="البريد الإلكتروني"
+        {...register("name")}
+        error={errors.email?.message}
+      />
+      <InputField
+        type="email"
+        placeholder="البريد الإلكتروني"
+        {...register("email")}
+        error={errors.email?.message}
+      />
+      <InputField
+        type="password"
+        placeholder="كلمة المرور"
+        {...register("password")}
+        error={errors.email?.message}
+      />
+      <InputField
+        type="password"
+        placeholder="تأكيد كلمة المرور"
+        {...register("confirmPassword")}
+        error={errors.email?.message}
+      />
+      <Button type="submit" loading={loading}>
+        تسجيل الدخول
+      </Button>
 
-          <div className="space-y-3">
-            <input
-              {...register("name")}
-              placeholder="الاسم الكامل"
-              className="input-base"
-            />
-            {errors.name && (
-              <p className="text-red-400 text-sm">{errors.name.message}</p>
-            )}
-
-            <input
-              {...register("email")}
-              type="email"
-              placeholder="البريد الإلكتروني"
-              className="input-base"
-            />
-            {errors.email && (
-              <p className="text-red-400 text-sm">{errors.email.message}</p>
-            )}
-
-            <div className="relative">
-              <input
-                {...register("password")}
-                type={showPassword ? "text" : "password"}
-                placeholder="كلمة المرور"
-                className="input-base pr-10"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute inset-y-0 left-3 flex items-center text-gray-500 hover:text-[#f9bc43b4] transition-colors">
-                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-              </button>
-            </div>
-            {errors.password && (
-              <p className="text-red-400 text-sm">{errors.password.message}</p>
-            )}
-          </div>
-          <input
-            {...register("password")}
-            type={showPassword ? "text" : "password"}
-            placeholder="تأكيد كلمة المرور"
-            {...register("confirmPassword")}
-            className="input-base pr-10"
-          />
-
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full py-2 btn-login ">
-            {loading ? "جاري الإنشاء..." : "إنشاء حساب"}
-          </button>
-          {message}
-          <p className="text-sm  text-[#832411]">
-            لديك حساب؟{" "}
-            <Link
-              href="/login"
-              className=" hover:border-b-[#832411] border-b-2 p-0 duration-250 transition-all font-bold border-[#83241100] ">
-              سجّل الدخول
-            </Link>
-          </p>
-        </form>
-      </div>
-    </div>
+      <p className="text-sm  text-[#832411]">
+        لديك حساب؟{" "}
+        <Link
+          href="/login"
+          className=" hover:border-b-[#832411] border-b-2 p-0 duration-250 transition-all font-bold border-[#83241100] ">
+          سجّل الدخول
+        </Link>
+      </p>
+    </FormContainer>
   );
 };
 
